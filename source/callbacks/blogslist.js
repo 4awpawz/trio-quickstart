205bb255ef752ca69b138384301419f84b99f43e
Jeff Schwartz <jefftschwartz@gmail.com>
2018-07-16 19:23:42 -0400

    added baseUrl to options

--- source/callbacks/blogslist.js
+++ source/callbacks/blogslist.js
@@ -5,5 +5,5 @@ module.exports = ($, siteMetaData) => {
     console.log("article count", articles.length);
 
     articles.forEach(article => $("ul.articles")
-        .append(`<li><a href="${article.url}">${article.title}</a></li>`));
+        .append(`<li><a data-trio-link href="${article.url}">${article.title}</a></li>`));
 };
