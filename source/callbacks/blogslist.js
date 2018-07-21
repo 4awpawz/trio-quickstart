0844e99c231aa93c6a75f27a15b0389dd1ee7490
Jeff Schwartz <jefftschwartz@gmail.com>
2018-07-21 17:00:42 -0400

    wip

--- source/callbacks/blogslist.js
+++ source/callbacks/blogslist.js
@@ -1,8 +1,9 @@
 const { join, normalize } = require("path");
 
 module.exports = ($, siteMetaData) => {
-    const articles = siteMetaData.filter(md => md.destPath.startsWith(`${join("public", "blog", "articles")}`));
-    console.log("article count", articles.length);
+    const articles = siteMetaData.filter(md =>
+        md.destPath.startsWith(`${join("public", "blog")}`) &&
+        md.destPath !== `${join("public", "blog", "index.html")}`);
 
     articles.forEach(article => $("ul.articles")
         .append(`<li><a data-trio-link href="${article.url}">${article.title}</a></li>`));
