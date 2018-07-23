18f5dfca9bc17f5e0f82518fe8dc698bdd22e375
Jeff Schwartz <jefftschwartz@gmail.com>
2018-07-23 19:22:02 -0400

    wip

--- source/callbacks/blogslist.js
+++ source/callbacks/blogslist.js
@@ -1,6 +1,19 @@
 const { join, normalize } = require("path");
 
 module.exports = ($, frag, siteMetaData) => {
-    siteMetaData.articlesCatalog.forEach(article => $("ul.articles")
-        .append(`<li><a data-trio-link href="${article.url}">${article.title}</a></li>`));
+    let parts = frag.url.split("/");
+    let n = parts.length > 2 ? parseInt(parts[parts.length - 1]) - 1 : 0;
+    let i = siteMetaData.paginate * n;
+
+    // generate list of articles for this blog page
+    siteMetaData.articlesCatalog
+        .slice(i, i + siteMetaData.paginate)
+        .forEach(article =>
+            $("ul.articles")
+                .append(`<li><a data-trio-link href="${article.url}">${article.title}</a></li>`)
+        );
+
+    // generate the next anchor tag
+    // $("a.next-blog-page").
+    // if(i + )
 };
