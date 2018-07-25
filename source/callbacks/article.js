e2301bdfaa24e949af95835ec1f1383a26722856
Jeff Schwartz <jefftschwartz@gmail.com>
2018-07-25 12:17:41 -0400

    wip

--- source/callbacks/article.js
+++ source/callbacks/article.js
@@ -1,3 +1,4 @@
 module.exports = ($, frag, frags) => {
     $("h1").append(frag.title);
+    $("div.breadcrumbs").append(`<a href="${frag.blogPageUrl}">${frag.blogPageUrl}</a>`);
 }
--- source/callbacks/blogslist.js
+++ source/callbacks/blogslist.js
@@ -6,4 +6,16 @@ module.exports = ($, frag, siteMetaData) => {
             $("ul.articles")
                 .append(`<li><a data-trio-link href="${article.url}">${article.title}</a></li>`)
         );
+    const $prevAnchorTag = $("a.prev-page");
+    const $nextAnchorTag = $("a.next-page");
+    if (frag.prevPageUrl) {
+        $prevAnchorTag.attr("href", frag.prevPageUrl);
+    } else {
+        $prevAnchorTag.addClass("prev-page--hidden")
+    }
+    if (frag.nextPageUrl) {
+        $nextAnchorTag.attr("href", frag.nextPageUrl);
+    } else {
+        $nextAnchorTag.addClass("next-page--hidden")
+    }
 };
