f39d1a165ac82fd95d6ef361ea9dea4b9e02610b
Jeff Schwartz <jefftschwartz@gmail.com>
2018-07-25 13:18:50 -0400

    wip

--- source/callbacks/article.js
+++ source/callbacks/article.js
@@ -1,4 +1,4 @@
 module.exports = ($, frag, frags) => {
     $("h1").append(frag.title);
-    $("div.breadcrumbs").append(`<a href="${frag.blogPageUrl}">${frag.blogPageUrl}</a>`);
+    $("div.breadcrumbs").append(`<a data-trio-link href="${frag.blogPageUrl}">${frag.blogPageUrl}</a>`);
 }
