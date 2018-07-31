c40e6f7981f7834fcf7abc96858bfed80f8f3fa0
Jeff Schwartz <jefftschwartz@gmail.com>
2018-07-31 17:34:14 -0400

    some refactoring

--- source/callbacks/showCurrentPageInHeader.js
+++ source/callbacks/showCurrentPageInHeader.js
@@ -1,11 +1,7 @@
-const showCurrentPageInHeader = ($, frag) => {
+module.exports = ($, frag) => {
     const activeHeaderItem = frag.matter.data.activeHeaderItem;
-    // if (!activeHeaderItem) {
-    //     throw new Error("All pages requre the \"activeHeaderItem\" property defined in its fragment's front-matter");
-    // }
-    if (activeHeaderItem) {
-        $(`li.header__nav-item:nth-child(${activeHeaderItem})`).addClass("header__nav-item--active");
+    if (!activeHeaderItem) {
+        throw new Error("Error: \"activeHeaderItem\" property not found in fragment");
     }
+    $(`li.header__nav-item:nth-child(${activeHeaderItem})`).addClass("header__nav-item--active");
 };
-
-module.exports = showCurrentPageInHeader;
