9da669002764aaa140c3c196830b77d1b283ccf6
Jeff Schwartz <jefftschwartz@gmail.com>
2018-09-11 12:03:59 -0400

    blogFolderName now retreived from userConfig

--- source/callbacks/setBlogFolderName.js
+++ source/callbacks/setBlogFolderName.js
@@ -1,4 +1,3 @@
 module.exports = ($, frag, siteMetaData) => {
-    const blogFolderName = frag.matter.data.blogFolderName;
-    $("#trio-blog-link").attr("href", `/${frag.matter.data.blogFolderName}`);
+    $("#trio-blog-link").attr("href", `/${siteMetaData.userConfig.blogFolderName}`);
 };
